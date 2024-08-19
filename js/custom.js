79f207a086661c5be3f6f7ef406fc6ce94c727bd
Yash Singh <77584751+yashsingh-0@users.noreply.github.com>
2024-08-20 02:05:20 +0530

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -54,14 +54,14 @@ var customScripts = {
 		if($('#main-nav ul li:first-child').hasClass('active')){
             // Glass Morphism, Header MainNav ::: PP69
 					$('#main-nav').css({
-                        "background": "rgba(100,108,254,0.45)",
-
+                        "background": "rgba(100,108,254,0.1)",
+                        "margin-top": "15px",
                         // rgba( 186, 114, 255, 0.45 )
-                        "box-shadow": "0 8px 32px 0 rgba( 31, 38, 135, 0.37 )",
-                        "backdrop-filter": "blur( 3.5px )",
-                        "-webkit-backdrop-filter": "blur( 3.5px )",
-                        // "border-radius": "10px",
-                        // "border": "1px solid rgba( 255, 255, 255, 0.18 )"
+                        "box-shadow": "8px 8px 32px 2px rgba( 31, 38, 135, 0.37 )",
+                        "backdrop-filter": "blur( 50px )",
+                        "-webkit-backdrop-filter": "blur( 100px )",
+                        "border-radius": "10px",
+                        "border": "1px solid rgba( 255, 255, 255, 0.18 )"
                     });
 		}
         $('#mainNav').onePageNav({        
